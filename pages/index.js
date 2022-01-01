ba7c2cca43c2c8f0aa65c31205c983e88dc8068a
venkyjayasurya <venkyjayasurya@gmail.com>
2022-01-02 03:27:10 +0530

    added article, list grid components

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
-        {/* for all the meta tags */}
         <title>NextJs</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="keywords" content="webd, programming" />
       </Head>
-
-      <h1>Home</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
