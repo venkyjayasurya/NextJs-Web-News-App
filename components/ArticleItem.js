77b5fcbb890a1151fe047ae11223384ff05812e0
venkyjayasurya <venkyjayasurya@gmail.com>
2022-01-02 03:30:51 +0530

    added all styles

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,10 +1,10 @@
-import articleStyles from "../styles/Article.module.css";
+import styles from "../styles/Article.module.css";
 import Link from "next/link";
 
 const ArticleItem = ({ article }) => {
   return (
     <Link href="/article/[id]" as={`/article/${article.id}`}>
-      <a className={articleStyles.card}>
+      <a className={styles.card}>
         <h3>{article.title} &arr;</h3>
         <p>{article.body}</p>
       </a>
