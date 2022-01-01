3aa1a82336f3184ca20fc796ac8c39a0fecdcee5
venkyjayasurya <venkyjayasurya@gmail.com>
2022-01-02 02:57:25 +0530

    added style jsx

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import headerStyles from "../styles/Header.module.css";
+
+const Header = () => {
+  const x = 5;
+
+  return (
+    <div>
+      <h1 className="title">
+        <span>WebDev</span>News
+      </h1>
+      <style jsx>{`
+        .title {
+          color: ${x > 3 ? "red" : "blue"};
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Header;
